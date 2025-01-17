ff83955301e585ae906dbc1c8be8e425f75b339b
zzjrabbit <239873059@qq.com>
2025-01-17 22:01:37 +0800

    add folder support

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 use oib::{Files, ImageBuilder};
 use serde::Deserialize;
 use std::env;
-use std::path::PathBuf;
+use std::path::{absolute, PathBuf};
 
 #[derive(Deserialize)]
 struct Config {
     output: String,
     files: Vec<File>,
+    folders: Vec<Folder>,
 }
 
 #[derive(Deserialize)]
 struct File {
+    source: String,
+    dest: String,
+}
+
+#[derive(Deserialize)]
+struct Folder {
     source: String,
     dest: String,
 }
@@ -39,11 +46,28 @@ fn main() {
         }
     };
 
-    let files: Files = config
+    let mut files: Files = config
         .files
         .into_iter()
         .map(|file| (file.dest, PathBuf::from(file.source)))
         .collect();
+
+    config.folders.into_iter().for_each(|folder| {
+        for entry in walkdir::WalkDir::new(folder.source.clone()) {
+            if let Ok(file) = entry {
+                let file_source = file.path();
+
+                let folder_path = absolute(folder.source.clone()).unwrap();
+
+                let file_dest = PathBuf::from(folder.dest.clone())
+                    .join(file_source.strip_prefix(folder_path).unwrap());
+                files.insert(
+                    file_dest.to_str().unwrap().to_string(),
+                    file_source.to_path_buf(),
+                );
+            }
+        }
+    });
 
     let output_path = PathBuf::from(config.output);
     ImageBuilder::build(files, &output_path).expect("Failed to build UEFI disk image");
